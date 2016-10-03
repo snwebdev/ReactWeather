484ced6551bb3925ffa73d8327a33fd54c5a3fde
Stewart <stewart@snwebdev.com>
2016-10-03 14:05:22 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
-app.listen(3000, function(){
-  console.log("Express server is up on port 3000");
+app.listen(PORT, function(){
+  console.log("Express server is up on port " + PORT);
 })
